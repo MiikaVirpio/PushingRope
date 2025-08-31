cd7f0b73d82849e83c66333f24b3984da9da12d2
Miika Virpiö <virpiomi@arcada.fi>
2025-08-31 13:25:26 +0300

    Menu musics

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,3 +3,9 @@ class_name MainMenu
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _process(delta: float) -> void:
+	# Bind ESC to close game
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
